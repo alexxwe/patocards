e9bc887828f76205db6a60909817ba85c692f281
David M. Fajardo <davidmf99@gmail.com>
2022-06-21 16:43:16 +0200

    Card generator and examples

---
+++ src/components/Builder.tsx
@@ -0,0 +1,65 @@
+import Card from './Card'
+import { useFormInput } from '../hooks/useFormInput'
+
+export default function Builder() {
+    const card = {
+        title: useFormInput(),
+        type: useFormInput(),
+        url: useFormInput(),
+        stats: [useFormInput('1'), useFormInput('1'), useFormInput('1')]
+    }
+
+    const InputNumber = ({ idx, title }: { idx: number, title: string}) => (
+        <input
+            className='bg-zinc-700 p-2 h-10 rounded'
+            type='number'
+            min={1}
+            max={9}
+            {...card.stats[idx].inputProp}
+            placeholder={title}
+        />
+    )
+
+
+    return (
+        <>
+            <section className='w-96 grid grid-cols-2 gap-2 mb-10'>
+                <input
+                    className='bg-zinc-700 p-2 h-10 rounded'
+                    type='text'
+                    {...card.title.inputProp}
+                    placeholder='Title...'
+                />
+
+                <select className='bg-zinc-700 p-2 h-10 rounded' {...card.type.inputProp}>
+                    <option value='0' className='bg-red-700'>🔥 Fire</option>
+                    <option value='1' className='bg-green-700'>☘ Plant</option>
+                    <option value='2' className='bg-blue-700'>🌊 Water</option>
+                </select>
+
+                <input
+                    className='bg-zinc-700 p-2 h-10 rounded col-span-2'
+                    type='text'
+                    {...card.url.inputProp}
+                    placeholder='Image URL...'
+                />
+
+                <div className='col-span-2 grid grid-cols-3 gap-2'>
+                    <InputNumber idx={0} title={'Live (1-9)'} />
+                    <InputNumber idx={1} title={'Attack (1-9)'} />
+                    <InputNumber idx={2} title={'Defense (1-9)'} />
+                </div>
+            </section>
+
+            <Card
+                type={Number(card.type.inputProp.value)}
+                stats={[
+                    Number(card.stats[0].inputProp.value),
+                    Number(card.stats[1].inputProp.value),
+                    Number(card.stats[2].inputProp.value),
+                ]}
+                title={card.title.inputProp.value}
+                image={card.url.inputProp.value} />
+        </>
+    )
+}
---
+++ src/components/Card.tsx
@@ -0,0 +1,41 @@
+const types = [
+    'from-red-900 via-red-700 to-amber-700',
+    'from-green-900 via-green-700 to-emerald-700',
+    'from-blue-900 via-blue-700 to-indigo-700',
+]
+
+const Card = ({ type, stats, title, image }: { type: number; title: string; stats: number[]; image: string }) => (
+    <div
+        className={`bg-gradient-to-br ${types[type]} py-2 px-4 rounded`}
+        style={{ width: '315px', height: '450px' }}
+        title='Click to copy URL image'
+        onClick={() => navigator.clipboard.writeText(image)}
+    >
+        <article className='h-full py-2 px-2 flex flex-col'>
+            <h2 className='text-2xl pb-2'>{title}</h2>
+
+            <section
+                className='rounded h-full'
+                style={{
+                    backgroundImage: `url("${image}")`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                }}
+            ></section>
+
+            <footer className='pt-2 flex justify-around'>
+                <span>
+                    <i className='bi bi-suit-heart-fill pr-2'></i> {stats[0]}
+                </span>
+                <span>
+                    <i className='ra ra-sword pr-2'></i> {stats[1]}
+                </span>
+                <span>
+                    <i className='ra ra-shield pr-2'></i> {stats[2]}
+                </span>
+            </footer>
+        </article>
+    </div>
+)
+
+export default Card
